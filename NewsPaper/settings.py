438641b235071ae0f2a0ea29e41d2bec9fe50ea2
Виктория Рыбалко <viktoriarybalko@MacBook-Air-Viktoria.local>
2025-11-12 19:40:48 +0300

    first commit

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -102,32 +102,36 @@ STATIC_URL = 'static/'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-
     'formatters': {
         'console_debug': {
-            'format': '%(asctime)s - %(levelname)s - %(message)s',
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
         },
         'console_warning': {
-            'format': '%(asctime)s - %(levelname)s - %(pathname)s - %(message)s',
+            'format': '{asctime} {levelname} {pathname} {message}',
+            'style': '{',
         },
         'console_error': {
-            'format': '%(asctime)s - %(levelname)s - %(pathname)s - %(message)s\n%(exc_info)s',
+            'format': '{asctime} {levelname} {pathname} {message}\n{exc_info}',
+            'style': '{',
         },
         'general_file': {
-            'format': '%(asctime)s - %(levelname)s - %(module)s - %(message)s',
+            'format': '{asctime} {levelname} {module} {message}',
+            'style': '{',
         },
-        'error_file': {
-            'format': '%(asctime)s - %(levelname)s - %(message)s - %(pathname)s\n%(exc_info)s',
+        'errors_file': {
+            'format': '{asctime} {levelname} {message} {pathname}\n{exc_info}',
+            'style': '{',
         },
         'security_file': {
-            'format': '%(asctime)s - %(levelname)s - %(module)s - %(message)s',
+            'format': '{asctime} {levelname} {module} {message}',
+            'style': '{',
         },
-        'email_formatter': {
-            'format': '%(asctime)s - %(levelname)s - %(message)s - %(pathname)s',
+        'email': {
+            'format': '{asctime} {levelname} {message} {pathname}',
+            'style': '{',
         },
     },
-
-    # ... остальной код без изменений ...
     'filters': {
         'require_debug_true': {
             '()': 'django.utils.log.RequireDebugTrue',
@@ -136,73 +140,83 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugFalse',
         },
     },
-
     'handlers': {
-        'console': {
+        'console_debug': {
             'level': 'DEBUG',
             'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
-            'formatter': 'console_debug',
+            'formatter': 'console_debug'
         },
-        'file_general': {
+        'console_warning': {
+            'level': 'WARNING',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_warning'
+        },
+        'console_error': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_error'
+        },
+        'general_file': {
             'level': 'INFO',
             'filters': ['require_debug_false'],
             'class': 'logging.FileHandler',
             'filename': 'general.log',
-            'formatter': 'general_file',
+            'formatter': 'general_file'
         },
-        'file_errors': {
+        'errors_file': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
             'filename': 'errors.log',
-            'formatter': 'error_file',
+            'formatter': 'errors_file'
         },
-        'file_security': {
+        'security_file': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
             'filename': 'security.log',
-            'formatter': 'security_file',
+            'formatter': 'security_file'
         },
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler',
-            'formatter': 'email_formatter',
+            'formatter': 'email'
         },
     },
-
     'loggers': {
         'django': {
-            'handlers': ['console', 'file_general'],
+            'handlers': ['console_debug', 'console_warning', 'console_error', 'general_file'],
             'level': 'DEBUG',
-            'propagate': True,
+            'propagate': False,
         },
         'django.request': {
-            'handlers': ['file_errors', 'mail_admins'],
+            'handlers': ['errors_file', 'mail_admins'],
             'level': 'ERROR',
             'propagate': False,
         },
         'django.server': {
-            'handlers': ['file_errors', 'mail_admins'],
+            'handlers': ['errors_file', 'mail_admins'],
             'level': 'ERROR',
             'propagate': False,
         },
         'django.template': {
-            'handlers': ['file_errors'],
+            'handlers': ['errors_file'],
             'level': 'ERROR',
             'propagate': False,
         },
         'django.db.backends': {
-            'handlers': ['file_errors'],
+            'handlers': ['errors_file'],
             'level': 'ERROR',
             'propagate': False,
         },
         'django.security': {
-            'handlers': ['file_security'],
+            'handlers': ['security_file'],
             'level': 'INFO',
             'propagate': False,
         },
-    }
+    },
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
